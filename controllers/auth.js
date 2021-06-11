87ab12f85781b06c5ee9a7d4e4b9469d2682dd7d
ketanprakash <ketanprakash001@gmail.com>
2021-06-11 20:14:00 +0530

    Created proper folder structure and did some work on signup and signin

---
+++ controllers/auth.js
@@ -0,0 +1,20 @@
+const signup = (req, res) => {
+	// {
+	// 	firstname: "First", 
+	// 	lastname: "Last",
+	// 	email: "email@something.com",
+	// 	password: "password"
+	// }
+	const { firstname, lastname, email, password } = req.body;
+	console.log(firstname, lastname, email, password);
+	res.status(200).send("Data received");
+}
+
+const signin = (req, res) => {
+	const { email, password } = req.body;
+	console.log(email, password);
+	res.status(200).send("Data received");
+}
+
+module.exports.signup = signup;
+module.exports.signin = signin;
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
+const auth = require('./routes/auth');
+//all json data is pared on arrival
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send("<h1>Home page</h1>");
+    console.log("GET /");
 })
 
+app.use('/auth', auth);
+
+//creating server
 app.listen(port, () => {
     console.log("Server started on port", port);
 })
