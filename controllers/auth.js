bf6f9adbbccb5beffabd52600ca7206c66fe54a1
ketanprakash <ketanprakash001@gmail.com>
2021-06-12 02:42:16 +0530

    implemented json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const client = require('../configs/db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const signup = (req, res) => {
 	// {
 	// 	firstname: "First", 
@@ -9,21 +10,40 @@ const signup = (req, res) => {
 	// }
 	const { firstname, lastname, email, password } = req.body;
 	console.log(firstname, lastname, email, password);
-	bcrypt.hash(password, 9, (err, hash) => {
+	//check if the email already exists
+	client.query(`SELECT * FROM users WHERE email = '${email}'`, (err, data) => {
 		if (err){
 			res.status(500).json({
-				message: "Hashing Error!"
-			});
+				message: "Database Error!"
+			})
 		}
 		else{
-			client.query(`INSERT INTO users(firstname, lastname, email, password) VALUES('${firstname}', '${lastname}','${email}','${hash}')`);
-			console.log(hash);
-			res.status(200).json({
-				message: "Successfully added user into database!",
-				hash
-			});
+			if (data.rows.length === 0){
+				bcrypt.hash(password, 9, (err, hash) => {
+					if (err){
+						res.status(500).json({
+							message: "Hashing Error!"
+						});
+					}
+					else{
+						client.query(`INSERT INTO users(firstname, lastname, email, password) VALUES('${firstname}', '${lastname}','${email}','${hash}')`);
+						console.log(hash);
+						const token = jwt.sign({email: email},`${process.env.SECRET_KEY}`);
+						res.status(200).json({
+							message: "Successfully added user into database!",
+							token
+						});
+					}
+				});
+			}
+			else{
+				res.status(500).json({
+					message: "The email already exists"
+				})
+			}
 		}
-	});	
+		
+	})
 	// res.status(200).send("Data received");
 }
 
@@ -38,7 +58,7 @@ const signin = (req, res) => {
 			});
 		}
 		else{
-			if (data.rows.length == 0){
+			if (data.rows.length === 0){
 				res.status(404).json({
 					message: "Email id does not exist"
 				})
@@ -53,8 +73,10 @@ const signin = (req, res) => {
 					}
 					else{
 						if (result){
+							const token = jwt.sign({email: email},`${process.env.SECRET_KEY}`);
 							res.status(200).json({
-								message: "Logged in successfully"
+								message: "Logged in successfully",
+								token
 							});
 						}
 						else{
