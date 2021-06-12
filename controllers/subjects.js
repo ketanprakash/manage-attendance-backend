aa89ad7b9b78a3ae82461e46777129bb8a56ca71
ketanprakash <ketanprakash001@gmail.com>
2021-06-12 21:13:52 +0530

    added deletesubject and getsubjects route

--- controllers/subjects.js
+++ controllers/subjects.js
@@ -18,4 +18,39 @@ const addSubject = (req, res) => {
 	});
 }
 
+const deleteSubject = (req, res) => {
+	const email = req.email;
+	client.query(`DELETE FROM subjects WHERE id = ${req.params.subjectid} AND email = '${email}'`, (err, data) => {
+		if (err){
+			console.log(err);
+			res.status(500).json({
+				message: "Database Error!!"
+			})
+		}
+		else{
+			res.status(200).json({
+				message: "Subject deleted!"
+			})
+		}
+	})
+}
+
+const getSubjects = (req, res) => {
+	const email = req.email;
+	client.query(`SELECT * FROM subjects WHERE email = '${email}'`, (err, data) => {
+		if (err){
+			console.log(err);
+			res.status(500).json({
+				message: "Database Error!!"
+			})
+		}
+		else {
+			console.log(data.rows);
+			res.status(200).json(data.rows);
+		}
+	})
+}
+
 module.exports.addSubject = addSubject;
+module.exports.deleteSubject = deleteSubject;
+module.exports.getSubjects = getSubjects;
--- routes/subjects.js
+++ routes/subjects.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {verifyToken} = require('../middlewares/verifyToken')
-const {addSubject} = require('../controllers/subjects');
+const {addSubject, deleteSubject, getSubjects} = require('../controllers/subjects');
 
 router.post('/addsubject', verifyToken, addSubject);
+router.delete('/deletesubject/:subjectid', verifyToken, deleteSubject);
+router.get('/getsubjects', verifyToken, getSubjects);
 
 module.exports = router;
