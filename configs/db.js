f8c083cf74e8213299456d9855ee75b8c8982601
ketanprakash <ketanprakash001@gmail.com>
2021-06-13 13:47:28 +0530

    fixed a database error

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 const client = new Client({
-	connectionString: "postgres://eqsubpgh:T573eKx0LeLx7zWW4i4hsQL0qg5ZYwmd@john.db.elephantsql.com/eqsubpgh"
+	connectionString: `${process.env.PGURL}`
 })
 
 client.connect((err) => {
