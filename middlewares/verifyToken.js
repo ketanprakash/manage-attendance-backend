658dcc9500ffe9347b138075de85584831b47ffb
ketanprakash <ketanprakash001@gmail.com>
2021-06-21 14:22:12 +0530

    Small changes

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-	const token = req.headers.token;
-	console.log(req.headers.token);
+	const token = req.headers.Authorization;
+	console.log(req.headers.Authorization);
 	jwt.verify(token, `${process.env.SECRET_KEY}`, (err, decoded) => {
 		if (err) {
 			console.log(err);
